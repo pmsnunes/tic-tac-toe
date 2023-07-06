d4c0e4d735eb7dd2bbf334ddbad4d8386582a271
pmsnunes <nunes.pedroms@gmail.com>
2023-07-06 11:45:12 +0100

    Added game over detection

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,7 +2,17 @@ const prompt = require("prompt-sync")();
 
 const EMPTY_POSITION = " ";
 const PLAYER_X = "x";
-const PLAYER_O = "o"
+const PLAYER_O = "o";
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8], 
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function whoStarts() {
   const validPlayers = [PLAYER_X, PLAYER_O];
@@ -16,11 +26,10 @@ function whoStarts() {
 
 function askForNextMove(map) {
   const nextMove = parseInt(prompt("What is your next move? (0-8)\n>"));
-  if (map[nextMove] === EMPTY_POSITION && nextMove > 0 && nextMove < 9) {
+  if (map[nextMove] === EMPTY_POSITION && nextMove >= 0 && nextMove < 9) {
     return nextMove;
-  } 
-  console.error("Invalid position")
-  
+  }
+  console.error("Invalid position");
 }
 
 function printMap(map) {
@@ -33,10 +42,21 @@ function printMap(map) {
   console.log(`_________`);
 }
 
-function isGameOver(map){
-  if(map.includes(EMPTY_POSITION)){
-    return {isOver: false, winner: null}
+function isGameOver(map) {
+  WINNING_COMBOS.forEach((combo) => {
+    if (
+      ((map[combo[0]] === map[combo[1]]) === map[combo[2]]) !==
+      EMPTY_POSITION
+    ){
+      return {isOver: true, winner: map[combo[0]]}
+    }
+  });
+
+  if (!map.includes(EMPTY_POSITION)) {
+    return { isOver: true, winner: null };
   }
+
+  return {isOver: false, winner: null};
 }
 
 function ticTacToe() {
@@ -46,11 +66,19 @@ function ticTacToe() {
   console.log(`It is player ${currentPlayer}'s turn:`);
   printMap(map);
 
-  while(true){
+  while (true) {
     const nextMove = askForNextMove(map);
     map[nextMove] = currentPlayer;
     currentPlayer = currentPlayer === PLAYER_X ? PLAYER_O : PLAYER_X;
-    return;
+    const { isOver, winner } = isGameOver(map, currentPlayer);
+    if(isOver){
+      if(winner){
+        console.log(`The winner is ${winner}`)
+      } else {
+        console.log("Its a tie!")
+      }
+      break;
+    }
   }
 }
 
